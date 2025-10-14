6706aec20ff4d1a32fb4bb58b6c0ba8ead987f15
mtaha-sumerge <mtaha@sumerge.com>
2025-10-14 17:44:49 +0300

    Overridden external bean

--- dinjection/src/main/java/config/AppConfig.java
+++ dinjection/src/main/java/config/AppConfig.java
@@ -1,5 +1,5 @@
 package config;
-
+import model.Course;
 import org.springframework.context.annotation.Import;
 import recommender.CourseRecommender;
 import beans.CourseRecommenderImpl2;
@@ -7,6 +7,8 @@ import external.CourseRecommenderImpl4;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @Import(ExternalConfig.class)
@@ -22,5 +24,21 @@ public class AppConfig {
     public CourseRecommender courseRecommender4() {
         return new CourseRecommenderImpl4();
     }
+
+    // Overridden external bean
+//    @Bean("courseRecommenderImpl4")
+//    public CourseRecommender courseRecommender4() {
+//        return new CourseRecommender() {
+//            @Override
+//            public List<Course> recommendedCourses() {
+//                return List.of(
+//                        new Course("Overriden Mongo DB"),
+//                        new Course("Overriden Express"),
+//                        new Course("Overriden React"),
+//                        new Course("Overriden Node")
+//                );
+//            }
+//        };
+//    }
 
 }
